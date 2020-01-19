e60eca2581c29384d8ab6522193a8e935976e476
student8 <beata.zawidlak@gmail.com>
2020-01-19 12:15:28 +0100

    last change

--- src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
+++ src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
@@ -2,29 +2,44 @@ package pl.edu.agh.mwo.invoice;
 
 import java.math.BigDecimal;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 import pl.edu.agh.mwo.invoice.product.Product;
 
 public class Invoice {
-	private Collection<Product> products;
-
+	private Map<Product, Integer> products = new HashMap<>(); 
+	
+	
 	public void addProduct(Product product) {
-		// TODO: implement
+		this.addProduct(product, 1);
 	}
 
 	public void addProduct(Product product, Integer quantity) {
-		// TODO: implement
+		this.addProduct(product, quantity);
 	}
 
-	public BigDecimal getSubtotal() {
-		return null;
+	public BigDecimal getNetPrice() {
+		BigDecimal sum = new BigDecimal(0);
+		
+		for(Product product : this.products.keySet()) {
+			Integer quantity = this.products.get(product);
+			sum = sum.add(product.getPrice().multiply(new BigDecimal(quantity)));
+		}	
+		return sum;
 	}
 
 	public BigDecimal getTax() {
-		return null;
+	 // odejmuje cenne netto od ceny brutto
+		return getGrossPrice().subtract(this.getNetPrice());
 	}
 
-	public BigDecimal getTotal() {
-		return null;
+	public BigDecimal getGrossPrice() {
+		BigDecimal sum = new BigDecimal(0);
+		for(Product product : this.products.keySet()) {
+			Integer quantity = this.products.get(product);
+			sum = sum.add(product.getPriceWithTax().multiply(new BigDecimal(quantity)));
+		}	
+		return sum;// cenna netto plus podatek - zamiast ceny bierzemy podatek
 	}
 }
